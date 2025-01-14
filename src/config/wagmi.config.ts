17cdcdebd0731564bbe97e228e7ae5c52f39c5cb
vihsonic <black02430horse@gmail.com>
2025-01-14 17:02:30 +0100

    update rpc urls

--- src/config/wagmi.config.ts
+++ src/config/wagmi.config.ts
@@ -6,7 +6,11 @@ export const config = createConfig({
   chains: [mainnet, sepolia],
   connectors: [metaMask()],
   transports: {
-    [mainnet.id]: http(),
-    [sepolia.id]: http(),
+    [mainnet.id]: http(
+      "https://mainnet.infura.io/v3/32b6f0fc73394d69afba9d6db3a9d84e"
+    ),
+    [sepolia.id]: http(
+      "https://sepolia.infura.io/v3/e4b3b2781dd34bc4817a1221b8a3b50a"
+    ),
   },
 });
